a5db26b36342a7c1f405d08016c2fe4dcf6f7bdb
Suleiman Barakat <suli.barakat@gmail.com>
2024-12-10 23:10:37 -0500

    big speed improvement

--- drawsaface/drawsaface.go
+++ drawsaface/drawsaface.go
@@ -1,10 +1,12 @@
 package drawsaface
 
 import (
+	"bufio"
 	"fmt"
 	"image"
 	"image/png"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/nfnt/resize"
@@ -20,11 +22,13 @@ type Drawsaface struct {
 	Width  int
 	Height int
 	Frames []Frame
+	Writer *bufio.Writer
 }
 
 func Create(paths []string) *Drawsaface {
 	daf := Drawsaface{Width: 100, Height: 100}
 	daf.Frames = make([]Frame, 0)
+	daf.Writer = bufio.NewWriter(os.Stdout)
 
 	for _, s := range paths {
 		daf.Load(s)
@@ -66,10 +70,10 @@ func (daf *Drawsaface) Draw() {
 	for {
 		//w, h, err := getTerminalSize()
 		w, h, _ := term.GetSize(0)
-		DrawAsciiFrame(daf.Frames[i], 0, 0, w, h)
+		DrawAsciiFrame(daf.Writer, daf.Frames[i], 0, 0, w, h)
 
 		// TODO: proper framerate
-		time.Sleep(1000000)
+		time.Sleep(100000000)
 		i += 1
 		i = i % len(daf.Frames)
 	}
@@ -77,33 +81,59 @@ func (daf *Drawsaface) Draw() {
 
 // -----------------------------------------------------------------------------------------------------------------
 
-func DrawAsciiFrame(f Frame, x, y, width, height int) {
+// var writer := bufio.NewWriter(os.Stdout)
+
+func DrawAsciiFrame(w *bufio.Writer, f Frame, x, y, width, height int) {
 	//fmt.Printf("%v\n", f)
 
 	// TODO: Good opportunity to parallize
 	// TODO: gradient for every pixel to determine edge
 
+	time_start := time.Now()
 	new_image := resize.Resize(uint(width), uint(height), f, resize.NearestNeighbor)
+	time_image := time.Now()
 
-	buff := ""
-	buff += col.MoveCursor(0, 0)
+	// var string_mut sync.Mutex
+	// var wg sync.WaitGroup
+
+	var sb strings.Builder
+	sb.Grow(width * height * 50)
+
+	//buff := ""
+	sb.WriteString(col.MoveCursor(0, 0))
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			buff += col.MoveCursor(x, y)
+
 			r, g, b, a := new_image.At(x, y).RGBA()
 			r = r >> 8
 			g = g >> 8
 			b = b >> 8
 			a = a >> 8
 
+			sb.WriteString(col.MoveCursor(x, y))
+
 			if a < 200 {
-				buff += col.DrawBlank()
+				sb.WriteString(col.DrawBlank())
 			} else {
-				//buff += col.DrawBlock(int(r), int(g), int(b))
-				buff += col.DrawChar("x", int(r), int(g), int(b))
+				sb.WriteString(col.DrawBlock(int(r), int(g), int(b)))
+				sb.WriteString(col.DrawChar("x", int(r), int(g), int(b)))
 			}
+
 		}
 	}
 
-	fmt.Print(buff)
+	buff := sb.String()
+
+	// wg.Wait()
+
+	time_frame := time.Now()
+	fmt.Fprint(w, buff)
+	w.Flush()
+	time_buffer := time.Now()
+
+	// some timing code piped to stderr
+	duration_resize := time_image.Sub(time_start)
+	duration_frame := time_frame.Sub(time_image)
+	duration_printf := time_buffer.Sub(time_frame)
+	fmt.Fprintf(os.Stderr, "w: %d\th: %d\t\t-> %v\t%v\t%v\n", width, height, duration_resize, duration_frame, duration_printf)
 }
